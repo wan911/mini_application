50562ed8af017366b62f43089ff60cf653ef7b33
minori <minori.ap.wan@gmail.com>
2019-10-02 12:13:53 +0900

    Add logout function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_out_path_for(resource)
+    user_session_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   devise_for :users
   root 'tweets#index'
   resources :tweets
