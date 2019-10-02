a58be6fdea3dadfb2d031f5cddf9db516f34b209
minori <minori.ap.wan@gmail.com>
2019-10-03 01:22:43 +0900

    Add editing function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,6 +16,19 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
     tweet = Tweet.find(params[:id])
     tweet.destroy
