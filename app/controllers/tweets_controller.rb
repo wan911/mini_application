5aad3491da4097b792ef14d856213b9073586d01
minori <minori.ap.wan@gmail.com>
2019-10-02 20:14:09 +0900

    Add tweet model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
   def index
   end
+
+  def new
+  end
 end
---
+++ db/migrate/20191002110108_create_tweets.rb
@@ -0,0 +1,9 @@
+class CreateTweets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tweets do |t|
+      t.text  :tweet, null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
